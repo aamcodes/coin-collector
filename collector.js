9dcd254f3d2fc87f0312bd101a219cd5840f798d
aamcodes <aaronmendezcodes@gmail.com>
2023-02-23 12:36:12 -0500

    finished project.

--- collector.js
+++ collector.js
@@ -1,26 +1,23 @@
-// your code 
+// your code
 // require the coins array
+const coins = require('./coins');
 
 function coinCollector(coins) {
-  const onlyCoins = [];
+	const onlyCoins = [];
 
-  const desired = [
-    'quarter',
-    'dime',
-    'nickel',
-    'penny'
-  ];
+	const desired = ['quarter', 'dime', 'nickel', 'penny'];
 
-  for (let i = 0; i < coins.length; i++) {
-    const coin = coins[i];
-    if (desired.includes(coin)) {
-      onlyCoins.push(coins[i]);
-    }
-  }
-  return onlyCoins;
+	for (let i = 0; i < coins.length; i++) {
+		const coin = coins[i];
+		if (desired.includes(coin)) {
+			onlyCoins.push(coins[i]);
+		}
+	}
+	return onlyCoins;
 }
 
 coinCollector(coins);
 
 // your code
 // export the coinCollector function
+module.exports = coinCollector;
